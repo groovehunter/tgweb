7bcce173d6b7ec505b64bff19137db6bdb79c1c6
groovehunter <groovehunter@posteo.de>
2022-10-02 10:00:46 +0200

    checking message for photo

--- helfa_dev_bot/processors.py
+++ helfa_dev_bot/processors.py
@@ -7,6 +7,7 @@ from .bot import TelegramBot
 
 import logging
 lg = logging.getLogger('root')
+
 from datetime import datetime
 
 """
@@ -15,36 +16,75 @@ def hello_world(bot: TelegramBot, update: Update, state: TelegramState):
     bot.sendMessage(update.get_chat().get_id(), 'Hello!')
 """
 
+"""
+Next steps:
+check for reply_to attribute
+photos flat in table und message-id dazu?
+oder multi photo DB modell?
+
+unterscheiden in anwendungsfall; 
+ob tgweb oder ob verschenka 
+"""
+
 @processor(state_manager, from_states=state_types.All)
 def save_message(bot: TelegramBot, update: Update, state: TelegramState):
-  msg = 'Es ist ein Fehler aufgetreten beim Speichern.'
+  lg.debug("............................................")
+  lg.debug("save message processor started...")
+  msg = 'Ein Fehler ist aufgetreten: '
+  text_exists = False
   uptype = update.type()
+  lg.debug('update type: '+ uptype)
   chat = update.get_chat()
   chat_id = chat.get_id()
+  lg.debug('chat_id: '+ chat_id)
   if not uptype == 'message':
-    lg.debug('update type: ', uptype)
     bot.sendMessage(chat_id, msg)
     return
+  message = update.get_message()
+  message_id = message.message_id
+  lg.debug('message_id: '+ message_id)
 
   try:
-    message = update.get_message()
     text = message.get_text()
+    text_exists = True
+    lg.debug('text: '+text)
+  except:
+    text_exists = False
+    lg.debug('no text')
+
+  if not text_exists:
+    lg.debug('IF no text')
+    try:
+      photo = message.photo
+      caption = photo.caption
+    except:
+      msg += 'get photo failed.'
+      lg.debug(msg)
+    try:
+      doc = message.document
+      lg.debug('got doc okay')
+    except:
+      msg += 'get doc failed.'
+      lg.debug(msg)
+      #bot.sendMessage(chat_id, msg)
+      #return
+
+
+  try:
     ts = message.date
-    message_id = message.message_id
-    #lg.debug('milestone', message_id)
-    #lg.debug('chat_id', chat_id)
-    #lg.debug('ts', ts)
     tgchat = TelegramChat.objects.get(telegram_id=chat_id)
     author = getattr(message, 'from')
     #username = 'groovehunter'
     tguser, isnew = TelegramUser.objects.get_or_create(username=author.username)
+
     tgmessage, isnew = TelegramMessage.objects.get_or_create(
         message_id=message_id,
         author=tguser,
         group=tgchat,
     )
   except:
-    msg = 'part 1'
+    msg += 'Konnte nicht tgmessage objekt erstellen.'
+    lg.debug(msg)
     bot.sendMessage(chat_id, msg)
     return
 
@@ -56,6 +96,7 @@ def save_message(bot: TelegramBot, update: Update, state: TelegramState):
     msg = 'saved to DB: ' + text[:30] +'...'
     bot.sendMessage(chat_id, msg)
   except:
+    msg += 'Konnte attribute des tgmessage objekts nicht speichern.'
     bot.sendMessage(chat_id, msg)
     return
 
